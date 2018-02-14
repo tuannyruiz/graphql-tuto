93277cff0956cbcad5fb0ab524ca2ce273e268fe
Tuanny Ruiz <tuannyruiz@gmail.com>
2018-02-14 17:02:36 -0200

    add babel-watch

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import express from 'express';
 const server = express();
 
+server.get('/', (req, res) => {
+    res.send('<html> <head> <body> <h1> teste </h1> </body> </head> </html>');
+});
+
 server.listen(4000, () => {
-    console.log("Listening on port 4000");
+    console.log('Listening on port 4000');
 });
