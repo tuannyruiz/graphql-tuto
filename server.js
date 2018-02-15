0c7fec3b511e1e2a21e7e33faff55f799efca915
Tuanny Ruiz <tuannyruiz@gmail.com>
2018-02-15 10:30:47 -0200

    add schema.js

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 import express from 'express';
 import {graphqlExpress, graphiqlExpress} from 'apollo-server-express';
+import bodyParser from 'body-parser';
+import schema from './schema.js';
 const server = express();
 
+server.use('/graphiql', graphiqlExpress({
+    endpointURL: "/graphql",
+}));
 
+server.use('/graphql', bodyParser.json(), graphqlExpress({schema}));
 
 server.listen(4000, () => {
     console.log('Listening on port 4000');
